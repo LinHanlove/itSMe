549e842420bc0685816cf4373313ab8d0dcd7035
luwenbin <linhansong1008@gmail.com>
2025-12-20 18:05:03 +0800

    feat: 代码着色跟随主题变化

--- src/app/components/Layout/Navbar.tsx
+++ src/app/components/Layout/Navbar.tsx
@@ -6,20 +6,20 @@ import ToggleTheme from "@/app/components/ui/ToggleTheme";
 export default function Navbar() {
   return (
     <div className="right flex items-center gap-x-4 md:gap-x-8">
-      <Link href="/blog" title="Blog">
+      <Link href="/blog" title="Blog" className="cursor-target">
         <span className="hidden md:block">Blog</span>
-        <Icon icon="ri:article-line" className="block md:hidden" />
+        <Icon icon="ri:article-line" className="block md:hidden" /> 
       </Link>
-      <Link href="/projects" title="Projects">
+      <Link href="/projects" title="Projects" className="cursor-target">
         <span className="hidden md:block">Projects</span>
         <Icon icon="ri:lightbulb-line" className="block md:hidden" />
       </Link>
 
-      <Link href="/products" title="Products">
+      <Link href="/products" title="Products" className="cursor-target">
         <span className="hidden md:block">Products</span>
         <Icon icon="eos-icons:products-outlined" className="block md:hidden" />
       </Link>
-      <Link href="/interesting" title="Interesting">
+      <Link href="/interesting" title="Interesting" className="cursor-target">
         <span className="hidden md:block">Interesting</span>
         <Icon icon="solar:star-rings-broken" className="block md:hidden" />
       </Link>
